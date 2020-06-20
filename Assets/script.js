24bf7f2d088feb276969d64b56618c4aea038725
wolfdog27 <kylenicholasanderson@gmail.com>
2020-06-20 00:19:20 -0700

    trying to local store and maybe time but lost

---
+++ Assets/script.js
@@ -0,0 +1,47 @@
+
+
+// var currentDate = moment().format("[Today is] dddd, MMMM Do YYYY");
+// currentDate.text()
+// $("#currentDate").append(currentDate);
+
+
+function time () {
+    var currentDate = $("#currentDate");
+    currentDate.text(moment().format("[Today is] dddd, MMMM Do YYYY"));
+    $("#currentDate").append(currentDate);
+}
+time ();
+
+// function time() {
+
+// }
+
+
+var textBox9 = $("#09")
+var textBox10 = $("#10")
+var textBox11 = $("#11")
+var textBox12 = $("#12")
+var textBox13 = $("#13")
+var textBox14 = $("#14")
+var textBox15 = $("#15")
+var textBox16 = $("#16")
+var textBox17 = $("#17")
+
+localStorage.setItem("text9", textBox9)
+localStorage.setItem("text10", textBox10)
+localStorage.setItem("text11", textBox11)
+localStorage.setItem("text12", textBox12)
+localStorage.setItem("text13", textBox13)
+localStorage.setItem("text14", textBox14)
+localStorage.setItem("text15", textBox15)
+localStorage.setItem("text16", textBox16)
+localStorage.setItem("text17", textBox17)
+
+
+// function storedText (){
+//     event.preventDefault();
+
+//     var textBox = $(text)
+
+
+// }
