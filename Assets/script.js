9742de6547f956ad046ffbb1c8c1d4bbd3cf3956
wolfdog27 <kylenicholasanderson@gmail.com>
2020-06-20 00:34:17 -0700

    all my js is saying not defined gotta go to bed and try tomorrow

--- Assets/script.js
+++ Assets/script.js
@@ -1,41 +1,52 @@
-
+// EVERYHTHING IS SAY THAT IT IS NOT DEFINED!!!!! =(
 
 // var currentDate = moment().format("[Today is] dddd, MMMM Do YYYY");
 // currentDate.text()
 // $("#currentDate").append(currentDate);
 
 
-function time () {
-    var currentDate = $("#currentDate");
-    currentDate.text(moment().format("[Today is] dddd, MMMM Do YYYY"));
-    $("#currentDate").append(currentDate);
-}
-time ();
+// function time () {
+//     var currentDate = $("#currentDate");
+//     currentDate.text(moment().format("[Today is] dddd, MMMM Do YYYY"));
+//     $("#currentDate").append(currentDate);
+// }
+// time ();
 
 // function time() {
 
 // }
 
+// var currentDate = moment().format("[Today is] dddd, MMMM Do YYYY");
 
-var textBox9 = $("#09")
-var textBox10 = $("#10")
-var textBox11 = $("#11")
-var textBox12 = $("#12")
-var textBox13 = $("#13")
-var textBox14 = $("#14")
-var textBox15 = $("#15")
-var textBox16 = $("#16")
-var textBox17 = $("#17")
 
-localStorage.setItem("text9", textBox9)
-localStorage.setItem("text10", textBox10)
-localStorage.setItem("text11", textBox11)
-localStorage.setItem("text12", textBox12)
-localStorage.setItem("text13", textBox13)
-localStorage.setItem("text14", textBox14)
-localStorage.setItem("text15", textBox15)
-localStorage.setItem("text16", textBox16)
-localStorage.setItem("text17", textBox17)
+// var textBox9 = $("#09");
+// var textBox10 = $("#10");
+// var textBox11 = $("#11");
+// var textBox12 = $("#12");
+// var textBox13 = $("#13");
+// var textBox14 = $("#14");
+// var textBox15 = $("#15");
+// var textBox16 = $("#16");
+// var textBox17 = $("#17");
+
+// localStorage.setItem("text9", textBox9);
+// localStorage.setItem("text10", textBox10);
+// localStorage.setItem("text11", textBox11);
+// localStorage.setItem("text12", textBox12);
+// localStorage.setItem("text13", textBox13);
+// localStorage.setItem("text14", textBox14);
+// localStorage.setItem("text15", textBox15);
+// localStorage.setItem("text16", textBox16);
+// localStorage.setItem("text17", textBox17);
+
+
+// $("#btn").on("click", function(event) {
+//     var hourSlot = $(this).attr("index");
+//     var hourText = $("#"+hourText).val();
+
+//     saveToLocalStorage(hourText,hourText); 
+//     });
+
 
 
 // function storedText (){
